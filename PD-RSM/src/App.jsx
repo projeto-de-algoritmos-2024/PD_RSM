86f86925db7a6adc3b19257e9685267a6231d1bf
pedroMADBR <pedrommartins.2014@gmail.com>
2024-07-29 22:55:39 -0300

    correção de exibição na página

--- PD-RSM/src/App.jsx
+++ PD-RSM/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <div id='head' >
+        Room Schedule Manager
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" id="taskName" placeholder="Nome da Tarefa" />
+        <input type="time" id="startTime" placeholder="Horário de Início" />
+        <input type="time" id="endTime" placeholder="Horário de Término" />
+        <button onclick="addTask()">Add Class</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Classes</h2>
+      <ul id="taskList"></ul>
+      <button onclick="sortTasksByEndTime()">Ordenar por Horário de Término</button>
     </>
   )
 }
