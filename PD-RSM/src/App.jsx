7094a658d39a4acbe483689016c4ac5e6f96882e
pedroMADBR <pedrommartins.2014@gmail.com>
2024-07-29 23:29:17 -0300

    Adição de método de ordenação

--- PD-RSM/src/App.jsx
+++ PD-RSM/src/App.jsx
@@ -1,25 +1,97 @@
 import { useState } from 'react';
-import './App.css'
-
+import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+
+  function addTask() {
+    if (taskName && startTime && endTime) {
+      const newTask = { taskName, startTime, endTime };
+      const updatedTasks = [...tasks, newTask].sort((a, b) => a.startTime.localeCompare(b.startTime));
+      setTasks(updatedTasks);
+      clearInputs();
+    }
+  }
+
+  function clearInputs() {
+    setTaskName('');
+    setStartTime('');
+    setEndTime('');
+  }
+
+  function removeTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+      const pivotIndex = partition(arr, left, right);
+      quickSort(arr, left, pivotIndex - 1);
+      quickSort(arr, pivotIndex + 1, right);
+    }
+  }
+
+  function partition(arr, left, right) {
+    const pivot = arr[right];
+    let i = left - 1;
+    for (let j = left; j < right; j++) {
+      if (arr[j].endTime.localeCompare(pivot.endTime) < 0) {
+        i++;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+    [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
+    return i + 1;
+  }
+
+  function sortTasksByEndTime() {
+    const sortedTasks = [...tasks];
+    quickSort(sortedTasks);
+    setTasks(sortedTasks);
+  }
 
   return (
     <>
-      <div id='head' >
+      <div id='head'>
         Room Schedule Manager
       </div>
       <div>
-        <input type="text" id="taskName" placeholder="Nome da Tarefa" />
-        <input type="time" id="startTime" placeholder="Horário de Início" />
-        <input type="time" id="endTime" placeholder="Horário de Término" />
-        <button onclick="addTask()">Add Class</button>
+        <input
+          type="text"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+          placeholder="Nome da Tarefa"
+        />
+        <input
+          type="time"
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+          placeholder="Horário de Início"
+        />
+        <input
+          type="time"
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+          placeholder="Horário de Término"
+        />
+        <button onClick={addTask}>Add Class</button>
       </div>
       <h2>Classes</h2>
-      <ul id="taskList"></ul>
-      <button onclick="sortTasksByEndTime()">Ordenar por Horário de Término</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className='task-item'>
+            {`${task.taskName} - Início: ${task.startTime} - Término: ${task.endTime}`}
+            <button onClick={() => removeTask(index)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={sortTasksByEndTime}>Ordenar por Horário de Término</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
